54a6d352b61fe5132d4031125a20abe79ebc0624
Ender Aydin Orak <ender.orak@johnlewis.co.uk>
2020-12-01 13:12:36 +0000

    Fix links

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
     <h2 className='text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-8 mt-8'>
       <Link href='/'>
         <img
-          src='/assets/images/m.png'
-          alt='meftun'
+          src='/assets/images/koders.png'
+          alt='koders'
           style={{ display: "inline-block", maxHeight: 50, margin: 0, padding: 0 }}
           className='cursor-pointer'
         />
--- components/intro.tsx
+++ components/intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
     <section className='flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12'>
       <h1 className='text-4xl md:text-4xl font-bold tracking-tighter leading-tight md:pr-8'>
-        <img src='/assets/images/koders.png' alt='koders' style={{ display: "inline-block", maxHeight: "120px" }} />
+        <a href="https://koders.co"><img src='/assets/images/koders.png' alt='koders' style={{ display: "inline-block", maxHeight: "120px" }} /></a>
       </h1>
       <h4 className='text-center md:text-left text-lg mt-5 md:pl-8'>Follow the Tech world with us.</h4>
     </section>
